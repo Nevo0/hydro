10c650f60e816cc0b9a72135aad6dc36e4ef9930
Nevo0 <Piecyk.rafal@gmail.com>
2019-03-13 22:20:04 +0100

    Navbar header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from "react"
 // import { Link } from "gatsby"
-import styled from "styled-components"
+// import styled from "styled-components"
 import Layout from "../components/layout"
 // import Image from "../components/image"
 import SEO from "../components/seo"
-import { FaBeer } from "react-icons/fa"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
